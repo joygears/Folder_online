3cf114f8c38ce5233bdefdbcc557b1fc6e10d775
2963663242 <2963663242@qq.com>
2022-09-25 01:32:51 +0800

    短期目标 代码清单16-2 中期目标  阅读练习完16章 长期目标学完c++反汇编与逆向分析技术揭秘

---
+++ 16/getProcessID/getProcessID.cpp
@@ -0,0 +1,54 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+
+#include <Windows.h>
+#include <tlhelp32.h>
+#include <iostream>
+
+
+
+
+DWORD GetPidByPName(const wchar_t* processName);
+
+int main()
+{
+	DWORD pid;
+	pid  = GetPidByPName(TEXT("Everything.exe"));
+	std::cout << pid;
+}
+
+
+/*
+	name:GetPidByPName
+	功能：通过进程名获取进程id
+	参数：const wchar_t* processName 进程名称
+	返回值： DWORD 若获取成功 返回 进程id, 若失败，或找不到则返回0
+*/
+DWORD GetPidByPName(const wchar_t* processName) {
+	LPPROCESSENTRY32 proEntry = { 0, };
+	HANDLE hSnapshot = 0;
+	DWORD th32ProcessID = 0;
+
+	hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	proEntry = new PROCESSENTRY32();
+	proEntry->dwSize = sizeof(PROCESSENTRY32);
+
+	if (Process32First(hSnapshot, proEntry)) {
+		if (lstrcmpi(proEntry->szExeFile, processName) == 0) { //当进程名相等的时候
+			th32ProcessID = proEntry->th32ProcessID;
+			delete proEntry;
+			return th32ProcessID;
+		}
+		while (Process32Next(hSnapshot, proEntry)) {
+			if (lstrcmpi(proEntry->szExeFile, processName) == 0) {
+				th32ProcessID = proEntry->th32ProcessID;
+				delete proEntry;
+				return th32ProcessID;
+			}
+		}
+	}
+	int error = ::GetLastError();
+	CloseHandle(hSnapshot);
+	delete proEntry;
+	return 0;
+}
+
