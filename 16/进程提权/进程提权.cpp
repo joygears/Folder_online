84cc0f90c7f255976232fb03f25533093c41dee4
2963663242 <2963663242@qq.com>
2022-10-07 15:15:43 +0800

    学习了进程提权，虽然不知道有什么用

---
+++ 16/进程提权/进程提权.cpp
@@ -0,0 +1,20 @@
+﻿// ConsoleApplication6.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <Windows.h>
+int main()
+{
+    TOKEN_PRIVILEGES NewState;
+    HANDLE TokenHandle;
+    HANDLE hProcess = GetCurrentProcess();
+    if (OpenProcessToken(hProcess, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &TokenHandle) != 0) {
+        if (LookupPrivilegeValueA(0, "SeDebugPrivilege", (PLUID)&NewState.Privileges)) {
+            AdjustTokenPrivileges(TokenHandle, 0, &NewState, 0, 0, 0);
+        }
+        CloseHandle(TokenHandle);
+    }
+    getchar();
+}
+
+
