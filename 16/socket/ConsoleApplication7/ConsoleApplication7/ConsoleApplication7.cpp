cf6b242688a53bd1807c2fc20ec027c9b5883445
2963663242 <2963663242@qq.com>
2022-10-17 01:16:50 +0800

    分析到调用虚表函数，根据命令选择分支

--- 16/socket/ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
+++ 16/socket/ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
@@ -5,8 +5,14 @@
 using namespace std;
 #pragma comment(lib,"ws2_32.lib")
 
+#define LEN_DATA 22
+
+void rc4_init(char* keyAry, char* key, int len);
+void rc4_cryp(char* keyAry, char* data, int len);
+
 int main(int argc, char* argv[]) {
     int i = 0x00408238;
+    char keyAry[256];
    
     float f = (float)0x0211DBD0;
    
@@ -66,11 +72,14 @@ int main(int argc, char* argv[]) {
         flag = 1;
        
         
-        string data;
-        getline(cin, data);
+        string data = {0x4B,0x75,0x47,0x6F,0x75,LEN_DATA,0x00,0x00,0x00,LEN_DATA,0x00,0x00,0x00,0x01,0x00,0x00,0x00,0x00,0x03,0x1C,0x00,0x00};
+        
+        rc4_init(keyAry, (char*)"1593044206", strlen("1593044206"));
+        rc4_cryp(keyAry, (char *)data.c_str(), LEN_DATA);
+        //getline(cin, data);
         const char* sendData;
         sendData = data.c_str();
-        send(clientSocket, sendData, strlen(sendData), 0);
+        send(clientSocket, sendData, LEN_DATA, 0);
         
         int num = recv(clientSocket, msg, 100, 0);
         if (num > 0)
@@ -90,4 +99,48 @@ int main(int argc, char* argv[]) {
 
 
     return 0;
+}
+
+
+
+void rc4_init(char* keyAry, char* key, int len) {
+    int  data[256] = { 0, };
+    int i = 0;
+    do {
+        keyAry[i] = i;
+        data[i] = key[i % len];
+        i++;
+    } while (i < 256);
+
+    int j = 0;
+    int mod = 0;
+    int temp = 0;
+    do {
+
+        mod += (unsigned char)keyAry[j] + data[j];
+        mod %= 256;
+        temp = keyAry[j];
+        //printf("%X\n", mod);
+        keyAry[j] = keyAry[mod];
+        keyAry[mod] = temp;
+        j++;
+    } while (j < 256);
+}
+
+void rc4_cryp(char* keyAry, char* data, int len) {
+
+    unsigned int i = 0, j = 0, k = 0, temp = 0;
+    if (len > 0) {
+        do {
+            j++;
+            j = j % 256;
+            temp = (unsigned char)keyAry[j];
+            k += temp;
+            k %= 256;
+            keyAry[j] = keyAry[k];
+            keyAry[k] = temp;
+            data[i] = data[i] ^ keyAry[((unsigned char)keyAry[j] + temp) % 256];
+            i++;
+        } while (i < len);
+    }
 }
