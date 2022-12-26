bdb3cc5bf64598e3b2923133432fc1c0d78dd830
a13576301231 <2963663242@qq.com>
2022-12-27 02:24:41 +0800

    还原了获取当前时间日期功能

--- qt/reverse_Sample_4_5/src/Sample_4_5.cpp
+++ qt/reverse_Sample_4_5/src/Sample_4_5.cpp
@@ -4,3 +4,15 @@ Sample_4_5::Sample_4_5(QWidget *parent){
 
 
 }
+
+void Sample_4_5::on_btnGetTime_clicked(){
+	QDateTime dataTime = QDateTime::currentDateTime();
+	ui.timeEditCurrent->setDateTime(dataTime);
+	
+	ui.lineEditTime->setText(dataTime.toString("HH:mm:ss"));
+	 ui.dateEdit->setDate(dataTime.date());
+	
+	 ui.lineEditDate->setText(dataTime.toString("yyyy-MM-dd"));
+	ui.dateTimeEdit->setDateTime(dataTime);
+	 ui.lineEditDateTime->setText(dataTime.toString("yyyy-MM-dd HH:mm:ss"));
+}
--- qt/reverse_Sample_4_5/src/Sample_4_5.h
+++ qt/reverse_Sample_4_5/src/Sample_4_5.h
@@ -6,9 +6,8 @@ class Sample_4_5:public QWidget
 	Q_OBJECT
 public:
 	Sample_4_5(QWidget *parent=nullptr);
-
-
-
+private slots:
+	void on_btnGetTime_clicked();
 private:
 	ui_Sample_4_5 ui; //0x30
 	QTimer timer; //0x190
