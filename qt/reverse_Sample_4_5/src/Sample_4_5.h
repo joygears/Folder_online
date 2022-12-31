9b59a493a5dbf3d23a9d44a7cf5bf2c82ebb3273
a13576301231 <2963663242@qq.com>
2022-12-31 14:19:10 +0800

    完成Sample_4_5逆向

--- qt/reverse_Sample_4_5/src/Sample_4_5.h
+++ qt/reverse_Sample_4_5/src/Sample_4_5.h
@@ -7,9 +7,17 @@ class Sample_4_5:public QWidget
 public:
 	Sample_4_5(QWidget *parent=nullptr);
 private slots:
-	void on_btnGetTime_clicked();
+	void on_btnGetTime_clicked(); // method index==0
+	void on_btnSetTime_clicked(); // method index==1
+	void on_btnSetDate_clicked(); // method index==2
+	void on_btnSetDateTime_clicked(); // method index==3
+	void  on_calendarWidget_selectionChanged(); // method index==4
+	void  on_btnSetInternal_clicked(); // method index==5
+	void  on_btnStart_clicked();
+	void  on_btnStop_clicked();
+	void displayTime();
 private:
 	ui_Sample_4_5 ui; //0x30
 	QTimer timer; //0x190
-	int m_1b0;
+	QTime time;
 };
