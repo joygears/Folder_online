1923149c9a00f5dfe5331446c074d8bb06589aa5
chengzhouliang <chengzhouliang@tenorshare.cn>
2023-04-18 09:31:45 +0800

    vector 迭代器原理研究

--- learn/std_learn/vector/vector.cpp
+++ learn/std_learn/vector/vector.cpp
@@ -8,26 +8,34 @@ class Student{
 public:
 	Student(string name){
 		m_name = name ;
-		cout <<"默认构造 " <<m_name << " " << this << endl;
+		//cout <<"默认构造 " <<m_name << " " << this << endl;
 	}
 	Student(Student & that){
 		m_name = that.m_name;
-		cout <<"复制构造 " <<m_name << " " << this << endl;
+		//cout <<"复制构造 " <<m_name << " " << this << endl;
 	}
 	~Student(){
 
-		cout <<"析构 " << m_name << " " << this << endl;
+		//cout <<"析构 " << m_name << " " << this << endl;
 	}
 private:
 	string m_name;
+	friend ostream& operator<<(ostream& os,Student& stu);
 };
 
+ostream& operator<<(ostream& os,Student& stu){
+
+	os << stu.m_name ;
+	return os;
+}
 
 int main(){
 	vector<Student> stus;
-	stus.push_back(Student("张三"));
-	stus.push_back(Student("李四"));
+	stus.push_back(Student("zhangsan"));
+	stus.push_back(Student("lisi"));
 
+	for(vector<Student>::iterator it = stus.begin();it != stus.end() ; ++it)
+		cout << *it << endl;
 
 	return 0;
 }
