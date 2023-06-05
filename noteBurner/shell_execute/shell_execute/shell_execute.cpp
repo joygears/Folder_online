b86282c405cddb40a88dfa6c00f726b8c19ea7a5
chengzhouliang <chengzhouliang@tenorshare.cn>
2023-06-05 19:33:20 +0800

    11

---
+++ noteBurner/shell_execute/shell_execute/shell_execute.cpp
@@ -0,0 +1,18 @@
+﻿// shell_execute.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <Shlobj.h>
+int main()
+{
+    LPCWSTR newPath = LR"(C:\Users\Administrator\AppData\Roaming\NoteBurner Netflix Video Downloader)";  // 新路径
+
+    BOOL result = SHSetFolderPathW(CSIDL_APPDATA, NULL, 0, newPath);
+    SetEnvironmentVariable(TEXT("APP_VMP_VERIFY"), TEXT("1"));
+    LoadLibrary(TEXT("cshell.dll"));
+    char ch;
+    std::cin >> ch;
+}
+
+
