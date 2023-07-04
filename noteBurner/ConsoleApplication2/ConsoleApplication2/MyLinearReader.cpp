b3351b6cd8644762564164207e804a82f2140e6c
chengzhouliang\Administrator <chengzhouliang@tenorshare.cn>
2023-07-04 17:57:08 +0800

    已经拿到kid，接下来获取iv

--- noteBurner/ConsoleApplication2/ConsoleApplication2/MyLinearReader.cpp
+++ noteBurner/ConsoleApplication2/ConsoleApplication2/MyLinearReader.cpp
@@ -33,6 +33,17 @@ AP4_Result MyLinearReader::ProcessMoof(AP4_ContainerAtom* moof, AP4_Position moo
 			
 		AP4_SampleDescription* sdescription = this->m_Trackers[0]->m_Track->GetSampleDescription(index);
 		if (sdescription->GetType() == AP4_SampleDescription::Type::TYPE_PROTECTED) {
+			
+			AP4_ProtectedSampleDescription* ProtectedSampleDescription = dynamic_cast<AP4_ProtectedSampleDescription*>(sdescription);
+			AP4_ProtectionSchemeInfo* SchemeInfo =  ProtectedSampleDescription->GetSchemeInfo();
+			AP4_ContainerAtom* SchiAtom = SchemeInfo->GetSchiAtom();
+			AP4_CencTrackEncryption * cenc = dynamic_cast<AP4_CencTrackEncryption *>(SchiAtom->GetChild(AP4_ATOM_TYPE_TENC));
+			if (!cenc) {
+
+				//SchiAtom->GetChild()  100D8019
+			}
+			const AP4_UI08*  kid = cenc->GetDefaultKid();
+			
 			printf("º”√‹∆¨∂Œ\n");
 		}
 		else {
