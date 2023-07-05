4a0134bd087cc266e49c82fcfd1d6e4397ef2c5a
chengzhouliang\Administrator <chengzhouliang@tenorshare.cn>
2023-07-05 15:11:55 +0800

    已成功还原处理iv，iv需要获取一个iv表，这个iv表是属于一个fragment的，每解密一个Fragment就需要获取这个表，从索引0开始获取，每解密一个Sample索引就+1

--- noteBurner/ConsoleApplication2/ConsoleApplication2/MyLinearReader.cpp
+++ noteBurner/ConsoleApplication2/ConsoleApplication2/MyLinearReader.cpp
@@ -1,5 +1,5 @@
 #include "MyLinearReader.h"
-
+#include <bento4/Ap4SencAtom.h>
 
 MyLinearReader::MyLinearReader(AP4_Movie& movie, AP4_ByteStream* fragment_stream ):AP4_LinearReader(movie, fragment_stream) {
 
@@ -33,17 +33,36 @@ AP4_Result MyLinearReader::ProcessMoof(AP4_ContainerAtom* moof, AP4_Position moo
 			
 		AP4_SampleDescription* sdescription = this->m_Trackers[0]->m_Track->GetSampleDescription(index);
 		if (sdescription->GetType() == AP4_SampleDescription::Type::TYPE_PROTECTED) {
-			
+
 			AP4_ProtectedSampleDescription* ProtectedSampleDescription = dynamic_cast<AP4_ProtectedSampleDescription*>(sdescription);
-			AP4_ProtectionSchemeInfo* SchemeInfo =  ProtectedSampleDescription->GetSchemeInfo();
+			AP4_ProtectionSchemeInfo* SchemeInfo = ProtectedSampleDescription->GetSchemeInfo();
 			AP4_ContainerAtom* SchiAtom = SchemeInfo->GetSchiAtom();
-			AP4_CencTrackEncryption * cenc = dynamic_cast<AP4_CencTrackEncryption *>(SchiAtom->GetChild(AP4_ATOM_TYPE_TENC));
+			AP4_CencTrackEncryption* cenc = dynamic_cast<AP4_CencTrackEncryption*>(SchiAtom->GetChild(AP4_ATOM_TYPE_TENC));
 			if (!cenc) {
 
 				//SchiAtom->GetChild()  100D8019
 			}
-			const AP4_UI08*  kid = cenc->GetDefaultKid();
-			
+			const AP4_UI08* kid = cenc->GetDefaultKid();
+			AP4_SencAtom* SencAtom = dynamic_cast<AP4_SencAtom*>(moof->GetChild(AP4_ATOM_TYPE_SENC));
+			AP4_CencSampleEncryption* CencSampleEncryption = (AP4_CencSampleEncryption*)(((char*)SencAtom) + 0x28);
+			AP4_CencSampleInfoTable* table;
+			if (!CencSampleEncryption || (CencSampleEncryption->GetOuter().GetFlags() & 1)==0){
+				AP4_UI08 DefaultPerSampleIvSize = cenc->GetDefaultPerSampleIvSize();
+				AP4_UI08 m_DefaultConstantIvSize = cenc->GetDefaultConstantIvSize();
+				AP4_UI08 m_DefaultCryptByteBlock = cenc->GetDefaultCryptByteBlock();
+				AP4_UI08 m_DefaultSkipByteBlock = cenc->GetDefaultSkipByteBlock();
+				const AP4_UI08* m_DefaultConstantIv = 0;
+				if (m_DefaultConstantIvSize) {
+					m_DefaultConstantIv = cenc->GetDefaultConstantIv();
+				}
+				if (CencSampleEncryption) {
+					CencSampleEncryption->CreateSampleInfoTable(0, m_DefaultCryptByteBlock, m_DefaultSkipByteBlock, DefaultPerSampleIvSize, m_DefaultConstantIvSize, m_DefaultConstantIv, table);
+				}
+			}
+		
+
+
+			//CencSampleEncryption->CreateSampleInfoTable(0,)
 			printf("º”√‹∆¨∂Œ\n");
 		}
 		else {
