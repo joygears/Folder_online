7dfa4bbf5782c605be92c83a57649395781ebedd
chengzhouliang\Administrator <chengzhouliang@tenorshare.cn>
2023-07-07 10:44:07 +0800

    日常学习

---
+++ learn/杂/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <future>
+
+// 异步任务，计算给定数字的平方
+int square(int num) {
+    return num * num;
+}
+
+int main() {
+ 
+    // 启动异步任务
+    std::future<int> futureResult = std::async(square, 5);
+
+    // 在主线程中执行其他操作...
+
+    // 获取异步任务的结果
+    int result = futureResult.get();
+
+    // 打印结果
+    std::cout << "平方结果: " << result << std::endl;
+
+    return 0;
+}
+
