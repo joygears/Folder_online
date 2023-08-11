86fbe43ede9bd3e8ba917a9c6fbede0a40cc35f6
2963663242 <2963663242@qq.com>
2023-08-11 16:01:59 +0800

    c++ 对象模型 成员函数指针

--- learn/Explore the c++ object model in depth/4.1/main.cpp
+++ learn/Explore the c++ object model in depth/4.1/main.cpp
@@ -15,7 +15,8 @@ class Point2d{
 			_x += rhs.x();
 			_y += rhs.y();
 		}
-		
+		virtual void ppp(float z){}
+		virtual float ppp(){return 1;}
 	protected:
 		float _x,_y;
 };
@@ -29,18 +30,52 @@ class Point3d : public Point2d
 			Point2d::operator+=(rhs);
 			_z += rhs.z();
 		}
-		
+		virtual float ppp(){return 1;}
+		virtual void ppp(float z){}
 	protected:
 		float _z;
 		
 };
-
+class Obj_Player
+{
+public:
+     int Sum()
+    {
+        return 1;
+    }
+     int Sum(int a)
+    {
+        return 2;
+    }
+	int num;
+};
 
 	int main(int argc,char ** argv){
 	
-	Point3d*  point= new  Point3d(1,2,3);
+	// Point3d*  point= new  Point3d(1,2,3);
+	// int (Obj_Player:: * Func1)() = (int (Obj_Player::*)())(&Obj_Player::Sum);
+	// float (Point3d:: * aa)() const = (float ( Point3d::* )() const)(&Point3d::z);
 	
-	point->Point3d::z();
+	// printf("&Point3d::z = %p" , aa);
+	// printf("&Obj_Player::Sum = %p" , Func1);
+	Obj_Player* obj = new Obj_Player;
+	int (Obj_Player:: * Func1)() = (int (Obj_Player::*)())(&Obj_Player::Sum);
+	int Obj_Player:: * num = &Obj_Player::num;
+
+	printf("obj->*Func1() = %d\n",(obj->*Func1)());
+	printf("Obj_Player:: * num = %d\n",obj->*num);
+	
 	return 0;
 }
 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+    // Obj_Player b;
+    // int (Obj_Player:: * Func1)() = (int (Obj_Player::*)())(&Obj_Player::Sum);
+    // (b.*Func1)();
+    // int (Obj_Player:: * Func2)(int) = (int (Obj_Player::*)(int))(&Obj_Player::Sum);
+    // (b.*Func2)(444);
+// }
