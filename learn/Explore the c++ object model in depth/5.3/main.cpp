47278658b43826d62bf088b7f495d903a003db88
2963663242 <2963663242@qq.com>
2023-08-17 21:12:37 +0800

    c++ 对象的效率

--- learn/Explore the c++ object model in depth/5.3/main.cpp
+++ learn/Explore the c++ object model in depth/5.3/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+
+#include <chrono>
 using namespace std;
 
 class Point {
@@ -9,7 +11,7 @@ class Point {
 		float _x,_y;
 };
 
-class Point3d :  public  Point {
+class Point3d :  public virtual Point {
 	public:
 		Point3d(float x = 0.0 , float y = 0.0, float z = 0.0):Point(x,y),_z(z){}
 
@@ -17,10 +19,76 @@ class Point3d :  public  Point {
 		float _z;
 };
 
+class Vertex :  public virtual Point {
+	public:
+		Vertex(Vertex * next = 0 ):_next(next){}
+		Vertex & operator=(const Vertex & v){
+			Point::operator=(v);
+			_next = v._next;
+			return *this;
+		}
+	protected:
+		Vertex *  _next;
+};
+
+class Vertex3D :  public  Point3d,public Vertex {
+	public:
+		Vertex3D(){}
+		Vertex3D & operator=(const Vertex3D & v){
+			Point::operator=(v);
+			Point3d::operator=(v);
+			Vertex::operator=(v);
+			return *this;
+		}
+	
+};
+
+
+template <typename Ret,class Fx,class... Args>
+Ret Time_Record(Fx func,Args... args){
+	
+	// 获取程序开始时间点
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    // 在这里放置你的代码，进行计算或处理
+	Ret ret = 	func(args...);
+    // 获取程序结束时间点
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    // 计算时间差
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+
+    // 输出运行时间
+    std::cout << "程序运行时间: " << duration << " 微秒" << std::endl;
+	return ret;
+}
+template <typename Fx,class... Args>
+void Time_Record(Fx func,Args... args){
+	
+	// 获取程序开始时间点
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    // 在这里放置你的代码，进行计算或处理
+		func(args...);
+    // 获取程序结束时间点
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    // 计算时间差
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+
+    // 输出运行时间
+    std::cout << "程序运行时间: " << duration << " 微秒" << std::endl;
+	
+}
+
+void _main(){
+	
+	
+}
 
 int main(int argc,char ** argv){
-	Point3d a,b;
-	a = b;
+	
+	Time_Record(_main);
 		
 	return 0;
 }
